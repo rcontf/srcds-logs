4f97b83c209a5de6d14e4270ef57f5acfb00b2ce
c43721 <rlahman.game@gmail.com>
2025-10-07 22:56:41 -0500

    feat: add docker tests

---
+++ tests/parser.test.ts
@@ -0,0 +1,12 @@
+import { Buffer } from "node:buffer";
+import assert from "node:assert";
+import { parsePacket } from "../src/parser.ts";
+
+Deno.test("Invalid packets return null", () => {
+    const packet = "bogus"
+    const encoded = new TextEncoder().encode(packet);
+
+    const result = parsePacket(Buffer.from(encoded));
+
+    assert.equal(result, null);
+});
